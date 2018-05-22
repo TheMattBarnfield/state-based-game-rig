1ffe1425f45af334b9bd36d5d99803fecaca6f5d
Matt Barnfield <matt.barnfield4@gmail.com>
2018-05-22 16:12:42 +0100

    add alpha-beta pruning

--- main.py
+++ main.py
@@ -6,13 +6,21 @@ from minimax_player import MinimaxPlayer
 
 def main():
     """Program entrypoint"""
+    matches = 10000
+    view = False
+    scores = [0, 0]
     game = Draughts()
-    state = game.new_game([MinimaxPlayer(2, MinimaxPlayer.draughts_count_heuristic), RandomPlayer()])
-    while game.evaluate(state) == 0:
-        if state['turn'] == 0:
-            print(game.display(state))
-        state = game.play_turn(state)
-    print('Game won by player ' + str(1-state['turn']))
+    player_one = RandomPlayer()
+    player_two = RandomPlayer()
+    for _ in range(matches):
+        state = game.new_game([player_one, player_two])
+        while game.evaluate(state) == 0:
+            if view and state['turn'] == 0:
+                print(game.display(state))
+            state = game.play_turn(state)
+        scores[1-state['turn']] += 1
+        print('Scores:'+ str(scores[0])+'-' + str(scores[1]))
+    print('Final scores:'+ str(scores[0])+'-' + str(scores[1]))
 
 if __name__ == '__main__':
     main()
--- minimax_player.py
+++ minimax_player.py
@@ -15,29 +15,36 @@ class MinimaxPlayer(Player):
         max_val = -2
         choice = -1
         for i, move in enumerate(game.get_moves(state)):
-            node_value = -self.max(game, game.perspective_change(move), 0)
+            node_value = -self.max(game, game.perspective_change(move), 0, max_val, -2)
             if node_value > max_val:
                 choice = i
                 max_val = node_value
         return choice
 
 
-    def max(self, game, state, depth):
+    def max(self, game, state, depth, alpha, beta):
         """
         Implements the minimax making use of the perspective_change funciton
         """
         value = game.evaluate(state)
         if not value == 0:
             return value
-        elif depth == self.depth:
+
+        moves = game.get_moves(state)
+
+        if len(moves) == 1:
+            return -self.max(game, game.perspective_change(moves[0]), depth, beta, alpha)
+
+        if depth == self.depth:
             return self.heuristic(state)
-        else:
-            max_val = -1
-            for move in game.get_moves(state):
-                min_val = -self.max(game, game.perspective_change(move), depth+1)
-                if min_val > max_val:
-                    max_val = min_val
-            return max_val
+        max_val = -1
+        for move in moves:
+            min_val = -self.max(game, game.perspective_change(move), depth+1, max_val, alpha)
+            if min_val < -alpha:
+                return min_val
+            if min_val > max_val:
+                max_val = min_val
+        return max_val
 
     @classmethod
     def draughts_count_heuristic(cls, state):
@@ -51,3 +58,40 @@ class MinimaxPlayer(Player):
                 elif piece is not DraughtsPiece.EMPTY:
                     enemies += 1
         return (allies-enemies)/(allies+enemies)
+
+    @classmethod
+    def draughts_count_and_king_heuristic(cls, state):
+        """same as count heuristic, but with kings worth double"""
+        allies = 0
+        enemies = 0
+        for row in state['board']:
+            for piece in row:
+                if piece is DraughtsPiece.ALLY:
+                    allies += 1
+                elif piece is DraughtsPiece.ALLY_KING:
+                    allies += 2
+                if piece is DraughtsPiece.ENEMY:
+                    enemies += 1
+                elif piece is DraughtsPiece.ENEMY_KING:
+                    enemies += 2
+        return (allies-enemies)/(allies+enemies)
+
+    @classmethod
+    def draughts_offensive_heuristic(cls, state):
+        """minimise number of opposing pieces"""
+        enemies = 0
+        for row in state['board']:
+            for piece in row:
+                if piece.is_enemy():
+                    enemies += 1
+        return 1-(2*enemies)/12
+
+    @classmethod
+    def draughts_defensive_heuristic(cls, state):
+        """minimise number of opposing pieces"""
+        allies = 0
+        for row in state['board']:
+            for piece in row:
+                if piece.is_ally():
+                    allies += 1
+        return (2*allies)/12 - 1
